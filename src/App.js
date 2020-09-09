bf870d377fe6f11524bd118cb0cda2025f3b05c9
zorac13 <kavithaajaymenon@gmail.com>
2020-09-10 03:36:32 +0530

    Added a shop page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 
+import ShopPage from "./pages/shop/shop.component";
 import HomePage from "./pages/homepage/homepage.component";
 import "./App.css";
 
@@ -9,6 +10,7 @@ function App() {
     <div>
       <Switch>
         <Route exact path="/" component={HomePage} />
+        <Route exact path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
---
+++ src/components/preview-collection/preview-collection.component.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+import CollectionItem from "../collection-item/collection-item.component";
+import "./preview-collection.styles.scss";
+
+const PreviewCollection = ({ title, items }) => (
+  <div className="preview-collection">
+    <h1 className="title">{title.toUpperCase()}</h1>
+    <div className="preview">
+      {items
+        .filter((item, idx) => idx < 4)
+        .map(({ id, ...otheritemProps }) => (
+          <CollectionItem key={id} {...otheritemProps} />
+        ))}
+    </div>
+  </div>
+);
+
+export default PreviewCollection;
