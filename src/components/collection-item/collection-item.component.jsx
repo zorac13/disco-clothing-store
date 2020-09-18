c275f3b90e817e2c9b1673b119ef3cd9cc3c3824
zorac13 <kavithaajaymenon@gmail.com>
2020-09-18 18:28:38 +0530

    updated shop with quantity finding feature

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,20 +1,33 @@
 import React from "react";
 
+import CustomButton from "../custom-buttons/custom-button.component";
+import { connect } from "react-redux";
+
 import "../collection-item/collection-item.styles.scss";
+import { addItem } from "../../redux/cart/cart.actions";
 
-const Collectionitem = ({ id, name, price, imageUrl }) => (
-  <div className="collection-item">
-    <div
-      className="image"
-      style={{
-        backgroundImage: `url(${imageUrl})`,
-      }}
-    />
-    <div className="collection-footer">
-      <span className="name">{name}</span>
-      <span className="price">{price}</span>
+const Collectionitem = ({ item, addItem }) => {
+  const { name, price, imageUrl } = item;
+  return (
+    <div className="collection-item">
+      <div
+        className="image"
+        style={{
+          backgroundImage: `url(${imageUrl})`,
+        }}
+      />
+      <div className="collection-footer">
+        <span className="name">{name}</span>
+        <span className="price">{price}</span>
+      </div>
+      <CustomButton onClick={() => addItem(item)} invert>
+        Add to Cart
+      </CustomButton>
     </div>
-  </div>
-);
+  );
+};
 
-export default Collectionitem;
+const mapDispatchToProps = (dispatch) => ({
+  addItem: (item) => dispatch(addItem(item)),
+});
+export default connect(null, mapDispatchToProps)(Collectionitem);
