518db6f1cb1085054dc36926b6c764191529aa77
zorac13 <kavithaajaymenon@gmail.com>
2020-09-23 20:19:25 +0530

    added remove item and add item in checkout page

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -22,6 +22,9 @@ const CheckoutPage = ({ cartItems, total }) => (
         <span>Quantity</span>
       </div>
       <div className="header-block">
+        <span>Price</span>
+      </div>
+      <div className="header-block">
         <span>Remove</span>
       </div>
     </div>
