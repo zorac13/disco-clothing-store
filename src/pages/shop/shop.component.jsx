cd5ab8892fe99bf753ab282a20c6a616de52aecf
zorac13 <kavithaajaymenon@gmail.com>
2020-09-28 08:10:07 +0530

    used selectors and shifted all the data in arrays to inside the  redux store

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,26 +1,21 @@
 import React from "react";
-import SHOP_DATA from "./shop.data";
+import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
 
 import PreviewCollection from "../../components/preview-collection/preview-collection.component";
 
-class ShopPage extends React.Component {
-  constructor(props) {
-    super(props);
+import { selectCollections } from "../../redux/shop/shop.selector";
 
-    this.state = {
-      collections: SHOP_DATA,
-    };
-  }
-  render() {
-    const { collections } = this.state;
-    return (
-      <div className="shop-page">
-        {collections.map(({ id, ...CollectionProps }) => (
-          <PreviewCollection key={id} {...CollectionProps} />
-        ))}
-      </div>
-    );
-  }
-}
+const ShopPage = ({ collections }) => (
+  <div className="shop-page">
+    {collections.map(({ id, ...CollectionProps }) => (
+      <PreviewCollection key={id} {...CollectionProps} />
+    ))}
+  </div>
+);
 
-export default ShopPage;
+const mapStateToProps = createStructuredSelector({
+  collections: selectCollections,
+});
+
+export default connect(mapStateToProps)(ShopPage);
